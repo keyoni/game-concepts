c59ad5f678a496ca83dff97adf4bcf681e78764c
[ccclend] <clendechy@csumb.edu>
2022-06-03 19:16:24 -0700

    Mouse Cursor Changer

---
+++ game-concepts-game/GameConcepts/Assets/Mouse/ToolSpriteChange.cs
@@ -0,0 +1,32 @@
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Mouse
+{
+    public class ToolSpriteChange : MonoBehaviour
+    {
+        public Texture2D cursorDefault;
+        public Texture2D cursorTexture;
+        
+        public Vector2 hotSpot = Vector2.zero;
+        public CursorMode cursorMode = CursorMode.Auto;
+        
+        void Start()
+        {
+            Cursor.SetCursor(null ,hotSpot,cursorMode);
+            cursorTexture = GetComponent<SpriteRenderer>().sprite.texture;
+           
+        }
+        private void OnMouseUp()
+        {
+                
+            Cursor.SetCursor(cursorTexture,hotSpot,cursorMode);
+        }
+
+        // private void OnMouseExit()
+        // {
+        //     Cursor.SetCursor(null, Vector2.zero, cursorMode);
+        // }
+    }
+}
